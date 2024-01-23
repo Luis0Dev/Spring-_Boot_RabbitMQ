b305150a8eadd36900954bee2f5ac77514fa1646
Illusion0dev <luisclaudio12023@gmail.com>
2024-01-23 12:35:19 -0300

    meu-primeiro-commit

---
+++ src/main/java/com/Illusion0DEV/SpringBootRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.Illusion0DEV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRabbitMqApplication.class, args);
+	}
+
+}
